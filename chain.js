0a12e86032fd93b8772875cb8705506e8ec722d1
Fluu <69867781+Fluu@users.noreply.github.com>
2020-10-11 14:03:02 -0400

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain {
+Constructor (bodyA,bodyB) {
+var options = {
+bodyA:bodyA,
+bodyB:bodyB,
+stiffness:0.004,
+length:8
+}
+this.chain = Constraint.create(options);
+World.add (world,this.chain);
+
+}
+display() {
+//var pointA = this.chain.bodyA.position;
+//var pointB = this.chain.bodyB.position;
+strokeWeight(4);
+//line(pointA.x,pointA.y,pointB.x,pointB.y);
+line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+}
+}
